3fb6a013ae0f85d078ca06873a15277ed2b2cbbb
Gilbert Marín Castillo <daviddlioncourt@gmail.com>
2021-04-10 19:46:09 -0600

    Se agrega nuevas pruebas de nn

--- Machine_Learning_Modelos_de_Clasificacion.R
+++ Machine_Learning_Modelos_de_Clasificacion.R
@@ -68,3 +68,86 @@ table(vino_prediccion, vino_real)
 #Calcular la exactitud
 mean(vino_prediccion == vino_real)
 
+#--------------------------------------------------------------------
+#COMPARACION CON DIFERENTE CANTIDAD DE VECINOs K
+#------------------------------------------------------------------
+
+k1 <- knn(train = Datos.Entrenamiento[-13], test = Datos.Prueba[-13], cl= tipo_vino)
+mean(vino_real == k1)
+
+k7 <- knn(train = Datos.Entrenamiento[-13], test = Datos.Prueba[-13], cl= tipo_vino, k=7)
+mean(vino_real == k7)
+
+k13 <- knn(train = Datos.Entrenamiento[-13], test = Datos.Prueba[-13], cl= tipo_vino, k=13)
+mean(vino_real==k13)
+
+
+#--------------------------------------------------------------------
+# OBTENER LAS PROBABILIDADES
+#----------------------------------------------------------------------
+
+vino_pred_prob <- k1 <- knn(train = Datos.Entrenamiento[-13], test = Datos.Prueba[-13], cl= tipo_vino, k=7, prob = TRUE)
+
+vino_prob <- attr(vino_pred_prob, "prob")
+
+head(vino_pred_prob)
+
+head(vino_prob)
+
+summary(vino_prob)
+
+
+#--------------------------------------------------------------------
+# NORMALIZAR LAS VARIABLES
+# ------------------------------------------------------------------
+
+normalizar <- function(x){
+  return((x-min(x))/(max(x)-min(x)))
+}
+
+Datos.Train.Scale <- normalizar(Datos.Entrenamiento[1:12])
+Datos.Train.Scale <- cbind(Datos.Train.Scale, Datos.Entrenamiento$type)
+
+colnames(Datos.Train.Scale)[13] <- "type"
+
+summary(Datos.Entrenamiento)
+summary(Datos.Train.Scale)
+
+table(Datos.Entrenamiento$type)
+table(Datos.Train.Scale$type)
+
+Datos.Test.Scale <- normalizar(Datos.Prueba[1:12])
+Datos.Test.Scale <- cbind(Datos.Test.Scale, Datos.Prueba$type)
+
+colnames(Datos.Test.Scale)[13] <- "type"
+
+# Repetir el analisis KNN 
+
+k7_scale <- knn(train = Datos.Train.Scale[-13], test = Datos.Test.Scale[-13], cl = tipo_vino, k=7)
+mean(vino_real == k7_scale)
+
+vino_prediccion_k7 <- knn(train = Datos.Entrenamiento[-13], test = Datos.Prueba[-13], 
+                          cl = tipo_vino, k=7)
+
+# Y crearemos una matriz de confusión
+
+table(vino_prediccion_k7, vino_real)
+
+vino_prediccion_k7_scale <- knn(train = Datos.Train.Scale[-13], test = Datos.Test.Scale[-13], 
+                                cl= tipo_vino, k=7, prob = TRUE)
+
+vino_prob_k7Scale <- attr(vino_prediccion_k7_scale, "prob")
+
+
+head(vino_prediccion_k7_scale)
+
+head(vino_prob_k7Scale)
+
+summary(vino_prob_k7Scale)
+
+
+# Comparacion contra datos normalizados
+
+vino_prediccion_k7_prueba <- knn(train = Datos.Train.Scale[-13], test = Datos.Test.Scale[-13], 
+                                 cl= tipo_vino, k=7)
+table(vino_prediccion_k7_prueba, vino_real)
